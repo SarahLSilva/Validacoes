d07217f13df878653c21a188212e3688deef92be
Sarah <sarah.lorena@alunoescolassociais.org.br>
2024-07-25 09:02:20 -0300

    criando arquivo

---
+++ validação de cpf/email.js
@@ -0,0 +1,11 @@
+//CÓDIGO DE VALIDAÇÃO DE EMAIL//---------------------------------------------------------------------------------------------------------//
+
+function checarEmail(){
+    if(document.forms[0].email.value === "" || document.forms[0].email.value.indexOf('@') == -1 || document.forms[0].email.value.indexOf('.') == -1){alert("Por favor, informe um e-mail válido");  return false;}
+
+    else{
+        alert("E-mail informado com sucesso!");
+    }
+    alert("E-mail informado");
+    document.getElementById('email').innerHTML = document.forms[0].email.value;
+}
